9c29e28b2b7e61364859b669e02f26b883b48c9e
Matteo Coni <matteo.coni99@gmail.com>
2023-04-05 11:01:55 +0200

    Aggiunti metodi per ricavare fv e ov

--- src/main/java/control/JiraController.java
+++ src/main/java/control/JiraController.java
@@ -1,6 +1,7 @@
 package control;
 
 import model.Release;
+import model.Issue;
 
 import java.io.*;
 import java.net.URL;
@@ -17,6 +18,7 @@ import org.json.JSONException;
 public class JiraController {
 
     private static final String MACRODATE = "releaseDate";
+    private static final String FIELD = "fields";
     public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
         InputStream is = new URL(url).openStream();
         try {
@@ -83,5 +85,87 @@ public class JiraController {
         }
 
         return releaseList;
+    }
+
+    public List<Issue> getIssues (String projName) throws IOException {
+
+        List<Release> releaseList = JiraController.getReleases(projName);
+        List<Issue> listIssues = new ArrayList<>();
+        List<Release> avList;
+        JSONArray avJSONArray;
+        Release ov;
+        Release fv;
+        Release iv;
+        Date resolutionDate;
+        Date openingDate;
+
+        Integer i=0;
+        Integer j=0;
+        Integer total=1;
+
+        do {
+            // only max 1000 issue each time
+            j = i + 1000;
+
+            /* project = <projName> AND issuetype = Bug AND (status = Closed OR status = Resolved) AND resolution = Fixed */
+            String url = "https://issues.apache.org/jira/rest/api/2/search?jql=project=%22"
+                    + projName + "%22AND%22issueType%22=%22Bug%22AND(%22status%22=%22closed%22OR"
+                    + "%22status%22=%22resolved%22)AND%22resolution%22=%22fixed%22&fields=key,resolutiondate,versions,created&startAt="
+                    + i.toString() + "&maxResults=" + j.toString();
+
+            JSONObject objectJson = readJsonFromUrl(url);
+            JSONArray issuesArray = objectJson.getJSONArray("issues");
+            total = objectJson.getInt("total");
+
+            for (; i < total && i < j; i++) {
+
+                /*Issue issue = createTicketInstance(i, issues, releasesList);
+                if(ticket != null) {
+                    ticketsList.add(ticket);
+                }*/
+                JSONObject currentObjectJson = issuesArray.getJSONObject(i%1000);
+                String key = currentObjectJson.get("key").toString();
+                String versionJson = currentObjectJson.getJSONObject(FIELD).get("versions").toString();
+                String fixVersionsJson = currentObjectJson.getJSONObject(FIELD).get("fixVersions").toString();
+                String stringResolutionDate = currentObjectJson.getJSONObject(FIELD).get("resolutiondate").toString();
+                String creationDate = currentObjectJson.getJSONObject(FIELD).get("created").toString();
+
+                //resolutionDate = resolutionDate.substring(0,10);
+                resolutionDate = Date.valueOf(stringResolutionDate);
+                openingDate = Date.valueOf(creationDate);
+
+                fv = getFixedOpeningVersions(releaseList, resolutionDate);
+                ov = getFixedOpeningVersions(releaseList, openingDate);
+
+                avJSONArray = issuesArray.getJSONObject(i%1000).getJSONObject(FIELD).getJSONArray("versions");
+                avList = getAffectedVersions(avJSONArray);
+
+            }
+
+        } while (i < total);
+
+        return listIssues;
+    }
+
+    public Release getFixedOpeningVersions(List<Release> releaseList, Date date) {
+
+        Release rel = null;
+        for (Release release : releaseList){
+            if(release.getDate().compareTo(date)>0){
+                rel = release;
+                break;
+            }
+        }
+
+        return rel;
+    }
+
+    public List<Release> getAffectedVersions(JSONArray affVersArray){
+
+        List<Release> avList = new ArrayList<>();
+        //to do: fai il parsing del jsonArray per creare le versioni ed aggiungerle alla lista avList
+
+
+        return avList;
     }
 }
